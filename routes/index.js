9c357dc2b1f45f0942965036bdf51aec45f9b21c
wllfengshu <wllfengshu@163.com>
2022-11-20 17:30:49 +0800

    增加首页

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login');
+  res.render('index');
 });
 
 router.get('/loginRePwd', function(req, res, next) {
